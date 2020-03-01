5f7433f89660d10d0be5091d78434e831416f952
SuvorkovRN <suvorkovRN@gmail.com>
2020-03-01 23:18:29 +0500

    Errors added 2

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -9,8 +9,6 @@ test('Ошибка есть', () => {
 
 test('Ошибка есть', () => {
   const errorsOfInput = new ErrorRepository();
-  errorsOfInput.errorRepository.set(1, 'Допускется ввод только чисел');
-  errorsOfInput.errorRepository.set(23, 'Допускется ввод только латинских символов');
   const received = errorsOfInput.translate(123);
   const expected = 'Unknown error';
   expect(received).toEqual(expected);
